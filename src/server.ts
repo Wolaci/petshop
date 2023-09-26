3e0e805c8791ee80f1872206e40666c17ca7cb20
Wolaci <wolacijose8@gmail.com>
2023-09-26 20:40:33 -0300

    Definindo arquitetura mvc

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+import mainRoutes from './routes/index';
 
 dotenv.config();
 
@@ -10,6 +11,13 @@ const server = express();
 server.set('view engine', 'mustache');
 server.set('views', path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
+
 server.use(express.static(path.join(__dirname, '../public')));
+
+server.use(mainRoutes);
+
+server.use((req, res)=>{
+    res.render('pages/404');
+});
 
 server.listen(process.env.PORT);
