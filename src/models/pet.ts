1142a405e6325664f9b0ec0d4aea09692b4e9ad8
Wolaci <wolacijose8@gmail.com>
2024-04-23 07:16:22 -0300

    Finalizando model

--- src/models/pet.ts
+++ src/models/pet.ts
@@ -1,5 +1,7 @@
+type PetType = 'dog' | 'cat' | 'fish';
+
 type Pet = {
-    type: 'dog' | 'cat' | 'fish',
+    type: PetType,
     image: string,
     name: string,
     color: string,
@@ -131,5 +133,13 @@ const data: Pet[] = [
 export const Pet = {
     getAll: (): Pet[] =>{
         return data;
+    },
+    getFromType: (type: PetType): Pet[] => {
+        return data.filter(item => item.type === type);
+    },
+    getFromName: (name: string): Pet[] =>{
+        return data.filter(item => 
+            item.name.toLowerCase().indexOf(name.toLowerCase()) > -1
+        );
     }
 };
