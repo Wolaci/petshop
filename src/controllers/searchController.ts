ad382cf5c83742fade7a1ff3cecfec6ff54bcc27
Wolaci <wolacijose8@gmail.com>
2024-04-24 06:51:53 -0300

    Busca e 404

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
+import { createMenuObject } from "../helpers/createMenuObject";
+import { Pet } from "../models/pet";
 
 export const search = (req: Request, res: Response)=>{
-    // res.send('pages/search);
+    let query: string = req.query.q as string;
+
+    if(!query){
+        res.redirect('/');
+        return;
+    }
+
+    let list = Pet.getFromName(query);
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    });
 }
